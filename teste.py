0e004a5e00be209bafd41c2b1e8ee78860073764
Izabella Araujo <l>
2025-08-02 19:46:43 -0300

    feat: add SQL Server connection functionality
    Implement database connection using pyodbc to establish and verify connection to SQL Server. The connection details are loaded from environment variables.

--- teste.py
+++ teste.py
@@ -1,7 +1,20 @@
 from dotenv import load_dotenv
 import os
+import pyodbc
 
 load_dotenv()
 
-database = os.getenv("DATABASE_PRD")
-print(database)
+def connection(server, database):
+    conn = pyodbc.connect(
+        f"DRIVER={{SQL Server}};SERVER={server};DATABASE={database};trusted_connection=yes"
+    )
+    return conn
+
+server = os.getenv("SERVER_PORTFOLIO")
+database = os.getenv("DATABASE_PORTFOLIO")
+conn = connection(server, database)
+
+if conn:
+    print("Conexão bem-sucedida!")
+else:
+    print("Erro ao conectar ao banco de dados.")
